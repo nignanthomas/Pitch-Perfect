0d9222f6abf44bd3120ad859ae022cddae9fc091
nignanthomas <nignanthomas@gmail.com>
2018-11-18 18:47:38 +0300

    create folder structure, db, initialize db

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+
+class Config:
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://thomas:psqltoto29@localhost/pitchperfect'
+
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+
+}
