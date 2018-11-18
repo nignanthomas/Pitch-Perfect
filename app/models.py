feaadb5548c70ebb54e7bd77f771acc38facb610
nignanthomas <nignanthomas@gmail.com>
2018-11-18 19:46:33 +0300

    create User Model in db

--- app/models.py
+++ app/models.py
@@ -1,44 +1,14 @@
-class Movie:
-    '''
-    Movie class to define Movie Objects
-    '''
+from . import db
 
-    def __init__(self,id,title,overview,poster,vote_average,vote_count):
-        self.id =id
-        self.title = title
-        self.overview = overview
-        self.poster = "https://image.tmdb.org/t/p/w500/" + poster
-        self.vote_average = vote_average
-        self.vote_count = vote_count
+from flask_login import UserMixin
 
 
-        
-class Review:
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
 
-    all_reviews = []
-
-    def __init__(self,movie_id,title,imageurl,review):
-        self.movie_id = movie_id
-        self.title = title
-        self.imageurl = imageurl
-        self.review = review
-
-
-    def save_review(self):
-        Review.all_reviews.append(self)
-
-
-    @classmethod
-    def clear_reviews(cls):
-        Review.all_reviews.clear()
-
-    @classmethod
-    def get_reviews(cls,id):
-
-        response = []
-
-        for review in cls.all_reviews:
-            if review.movie_id == id:
-                response.append(review)
-
-        return response
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255), index = True)
+    email = db.Column(db.String(255), unique=True, index=True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
