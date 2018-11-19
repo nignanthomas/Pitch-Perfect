43725bfdb0314a6c9a90a772180c81dd9b1e894b
nignanthomas <nignanthomas@gmail.com>
2018-11-19 11:12:42 +0300

    create RegistrationForm class in auth/forms.py; create registration view function in auth/views.py

--- app/models.py
+++ app/models.py
@@ -3,6 +3,13 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 
 from datetime import datetime
+from . import login_manager
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 
 
