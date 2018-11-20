346abc35ba86606d01609004d1d6543865849456
nignanthomas <nignanthomas@gmail.com>
2018-11-20 10:02:07 +0300

    update auth/views.py to add docstrings

--- app/auth/views.py
+++ app/auth/views.py
@@ -9,6 +9,10 @@ from ..email import mail_message
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
+    '''
+    View Function to login users
+    '''
+
     login_form = LoginForm()
     if login_form.validate_on_submit():
         user = User.query.filter_by(email = login_form.email.data).first()
@@ -16,7 +20,7 @@ def login():
             login_user(user,login_form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
 
-    flash('Invalid username or password /!\')
+    flash('Invalid username or password /!\\')
 
     title = "Pitch-Perfect -- Login Form"
     return render_template('auth/login.html',login_form = login_form,title = title)
@@ -25,6 +29,10 @@ def login():
 
 @auth.route('/register',methods = ["GET","POST"])
 def register():
+    '''
+    View Function to register new users
+    '''
+
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
@@ -35,15 +43,21 @@ def register():
 
         mail_message("Welcome to Pitch-Perfect","email/welcome_user",user.email,user=user)
 
-        return redirect(url_for('auth.login'))
         title = "Pitch-Perfect -- New Account"
-    return render_template('auth/register.html',registration_form = form)
+
+        return redirect(url_for('auth.login'))
+
+    return render_template('auth/register.html',registration_form = form, title = title)
 
 
 
 @auth.route('/logout')
 @login_required
 def logout():
+    '''
+    View Function to logout a user
+    '''
+
     logout_user()
 
     flash("You've been successfully registered!")
